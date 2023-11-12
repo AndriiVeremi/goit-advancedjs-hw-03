8bda9a9173a791d4235940359ecec3e1405f1458
Andrii Veremii <dashuk10@gmail.com>
2023-11-12 09:32:22 +0100

    add

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 const refs = {
   breedSelect: document.querySelector('.js-breed-select'),
   catInfo: document.querySelector('.cat-info'),
+  loader: document.querySelector('.loader'),
 };
 
 const slimSelect = new SlimSelect({
@@ -14,32 +15,35 @@ const slimSelect = new SlimSelect({
 
 async function onSelectCats() {
   try {
+    refs.loader.style.display = 'block';
     const catId = refs.breedSelect.value;
     const data = await fetchCatByBreed(catId);
     markupCats(data);
   } catch (error) {
     console.log(error);
   }
+  refs.loader.style.display = 'none';
 }
 
 function markupCats(data) {
   const imgCats = data[0].url;
   const infoCats = data[0].breeds[0];
- const cat = `<div class="wrapper">
-      <img class="img" src="${imgCats}" alt="${infoCats.name}"/>
+ const cat = `<div class="card">
+  <img class="img" src="${imgCats}" alt="${infoCats.name}"/>
       <div class="info">
         <h2 class="name">${infoCats.name}</h2>
         <p><b class="description">Description:</b> ${infoCats.description}</p>
         <p><b class="temperament">Temperament:</b> ${infoCats.temperament}</p>   
        </div>
-    </div>`;
+</div>`;
 
-refs.catInfo.insertAdjacentHTML('afterbegin', cat);
+  refs.catInfo.innerHTML = cat;
 }
 
 async function searchCats() {
   try {
     await fetchBreeds().then(breeds => {
+      refs.loader.style.display = 'block';
       const data = breeds.map(({ id, name }) => ({ value: id, text: name }));
       console.log(data);
       slimSelect.setData(Array.from(data));
@@ -48,6 +52,7 @@ async function searchCats() {
   } catch (error) {
     console.log(error);
   }
+  refs.loader.style.display = 'none';
 }
 
 searchCats();
